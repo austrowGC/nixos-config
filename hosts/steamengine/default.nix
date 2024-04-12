28dded5eeae94b4806735545eef28be844ffc839
Gregory Austrow <gregoryaustrow@gmail.com>
2024-04-12 13:12:36 -0400

    add boot and opengl

--- hosts/steamengine/default.nix
+++ hosts/steamengine/default.nix
@@ -5,6 +5,20 @@
     ./hardware-configuration.nix
   ];
 
+
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 3;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      timeout = 5;
+    };
+  };
+  hardware.opengl.enable = true;
   gnome.enable = true;
   games.enable = true;
 }
